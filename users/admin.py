ce4ddcb4b6c0daf7bb8bb4265a3128b90de02144
mrb76 <mrbakhtiari4@yahoo.com>
2022-07-16 15:05:07 +0430

    notifications admin implemented

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, NotificationType, Notification
 
 
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('text', 'user', 'writer', 'create_date_time',
+                    'modify_date_time', 'is_news')
+
+
+class NotificationTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create_date_time',)
+
+
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(NotificationType, NotificationTypeAdmin)
 admin.site.register(User, UserAdmin)
