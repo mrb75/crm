2f94d54e5dac3fbce3c4c4c9d33794980ad29541
mrb76 <mrbakhtiari4@yahoo.com>
2022-07-19 19:46:52 +0430

    change subusers permission and add userimage permissions

--- users/permissions.py
+++ users/permissions.py
@@ -25,3 +25,32 @@ class SubUsersDeletePermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.admin
+
+
+class UserImageViewPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.view_userimage')
+
+
+class UserImageAddPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.add_userimage')
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.admin or (request.user.admin == obj.admin)
+
+
+class UserImageChangePermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.change_userimage')
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.admin or (request.user.admin == obj.admin)
+
+
+class UserImageDeletePermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.delete_userimage')
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.admin
