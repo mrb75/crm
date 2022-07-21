fd6e86e3980cfbb54ebcf5e6389b155303b89379
mrb76 <mrbakhtiari4@yahoo.com>
2022-07-21 21:30:46 +0430

    ticket permissions implemented

--- users/permissions.py
+++ users/permissions.py
@@ -54,3 +54,29 @@ class UserImageDeletePermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user.admin
+
+
+class TicketViewPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.view_ticket')
+
+
+class TicketAddPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.add_ticket')
+
+
+class TicketChangePermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.change_ticket')
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
+
+
+class TicketRemovePermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.has_perm('users.delete_ticket')
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.user
